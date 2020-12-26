1fadf78c4c42565ab7d3d3b6957ad949625e9b5f
vtinney <vtinney@gwu.edu>
2020-12-26 14:02:15 -0500

    Update 07.compare.HR.GBD.R
    update

--- 07.compare.HR.GBD.R
+++ 07.compare.HR.GBD.R
@@ -24,3 +24,19 @@ df3 <- merge(df2, gbd.rate, by='WHORegion')
 
 write.csv(df3, 'compare.hr.gbd.csv')
 
+#============================================================================
+
+setwd('E:/GBD_2020/Final/results/')
+hr <- read.csv('allcauses.city.results.csv')
+names(hr) <- c("X","year","id","pop.sum.hr","popw.hr",          
+"city","cluster","location_id","GBDRegion","GBDSuperRegion", 
+"SDGRegion","WHORegion","WHOIncomeRegion","parent_id","ac.nohap.hr",       
+"ac.who.hr")
+
+setwd('E:/GBD_2020/Final/results/gbd/')
+gbd <- read.csv('allcauses.city.results.GBD.csv')
+
+df <- merge(hr, gbd, by=c('year','id','city','cluster','location_id','GBDRegion','GBDSuperRegion',
+                          'SDGRegion','WHORegion','WHOIncomeRegion'))
+
+write.csv(df,'compare.GBD.HR.city.csv')
